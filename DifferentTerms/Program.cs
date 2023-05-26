f31ae07663ba0141fef20e79ee16e6442184bc89
Ibrahim <mamedovarbeiter@gmail.com>
2023-05-26 21:24:09 +0300

    done

---
+++ DifferentTerms/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Int32 n = Convert.ToInt32(Console.ReadLine());
+
+        List<Int32> result = new List<Int32> { };
+
+        Int32 term = 1;
+        
+        while (n > 0)
+        {
+            if (n - term >= term + 1)
+            {
+                result.Add(term);
+                n -= term;
+            }
+            else
+            {
+                result.Add(n);
+                break;
+            }
+            
+            term++;
+        }
+
+
+        Console.WriteLine(result.Count);
+
+        foreach (Int32 element in result)
+        {
+            Console.Write(element + " ");
+        }
+    }
+}
